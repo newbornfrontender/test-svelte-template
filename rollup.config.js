a7633b73c884ac1d2534f7ddaf26b1784ae52a0a
Infant Frontender <newbornfrontender@gmail.com>
2019-06-03 08:08:32 +0300

    feat: add rollup-plugin-replace

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import preprocess from 'svelte-preprocess';
 import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -16,6 +17,9 @@ export default {
     file: 'public/bundle.js',
   },
   plugins: [
+    replace({
+      NODE_ENV: production && JSON.stringify('production'),
+    }),
     svelte({
       dev: !production,
       preprocess: preprocess({
